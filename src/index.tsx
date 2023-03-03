76814fd207fd62bd4605b5cea6ba667fe3c81cdc
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2023-03-03 18:54:24 +0000

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
 import Providers from "./components/Providers";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Providers>
       <App />
     </Providers>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
